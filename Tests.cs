72bdab0888ad3650d34720356421ebb907332f25
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-08-09 12:01:30 -0600

    Update Tests.cs

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -17,6 +19,27 @@ namespace LearnCSharpWithTests
             var array = Array.Empty<string>();
             var arrayLength = array.Length;
             arrayLength.Should().Be(0);
+        }
+
+        [Test]
+        public void DictionaryValues()
+        {
+            var dictionary = new Dictionary<string, string> {{"a", "alpha"}, {"b", "bravo"}};
+
+            foreach (var dictionaryValue in dictionary.Values)
+            {
+                Console.WriteLine(dictionaryValue);
+            }
+
+            dictionary.Values.Should().BeOfType<Dictionary<string, string>.ValueCollection>();
+        }
+
+        [Test]
+        public void LinqChaining()
+        {
+            List<string> names = new List<string> {"Joe", "Jane", "Jim"};
+            IOrderedEnumerable<string> result = names.OrderBy(x => x.Length);
+            result.Should().BeAssignableTo<IEnumerable<string>>();
         }
     }
 }
